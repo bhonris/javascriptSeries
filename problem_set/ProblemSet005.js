74ef2031c8492ad2473639d4c6b1bc2369592e69
Bhon Bunnag <bhonris@gmail.com>
2021-09-19 20:10:19 -0700

    Add problem set

---
+++ problem_set/ProblemSet005.js
@@ -0,0 +1,50 @@
+// Problem 1: Unique
+// Write a function which determins whether a string has all unique characters
+// (string) => boolean
+function isStringUnique(aString) {
+    return false
+}
+// isStringUnique("a") => true
+// isStringUnique("racecar") => false (2 r's, 2 a's, 2 c's)
+// isStringUnique("beauty") => true
+// isStringUnique("array") => false (2 r's)
+
+
+// Problem 2: URLify
+// Write a function that replaces all spaces in a string with '%20'
+// (string) => string
+function getURLString(aString){
+    return ""
+}
+// getURLString("Mr John Smith") => Mr%20John%20Smith
+
+
+// Problem 3: Remove duplicates
+// Remove all duplicates from a list
+// (array) => array
+function removeDuplicate(anArray){
+    return []
+}
+// removeDuplicate([1,3,4,6,3,7,4]) => [1,3,4,6,7]
+
+// Problem 4: is Palindrome
+// check to see whether a string is a palindrome or not
+// (string) => boolean
+function isPalindrome(aString){
+    return false
+}
+// isPalindrome("racecar") => true
+// isPalidrome("palindrome") => false
+
+// Problem 5: string compression
+// write a function that does string compression
+// (string) => [{letter: string, count: number}]
+function compressString(aString){
+    return {}
+}
+// compressString("aaabcccccddaaa") => 
+//     [{letter: a, count:3},
+//      {letter: b, count:1},
+//      {letter: c, count:5},
+//      {letter: d, count:2 },
+//      {letter: a, count:3}]
