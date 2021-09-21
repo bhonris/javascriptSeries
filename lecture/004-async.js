661c81ae19d5c9d3c86c78a5bae2868f4cd7a266
Bhon Bunnag <bhonris@gmail.com>
2021-09-20 20:52:28 -0700

    Lecture  content

--- lecture/004-async.js
+++ lecture/004-async.js
@@ -2,19 +2,26 @@ function sleep(ms){
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-async function loadANumber() {
-    console.log("start loading")
-    await sleep(3000)
-    console.log("finish loading")
-    return 42
-}
+// async function loadANumber() {
+//     console.log("start loading")
+//     await sleep(3000)
+//     console.log("finish loading")
+//     return 42
+// }
 
-async function resolveY() {
-    const x = loadANumber();
-    console.log(`value has returned: ${x}`)
-    return x + 1
-}
-let y = 5
+// async function resolveY() {
+//     const x = await loadANumber();
+//     console.log(`value has returned: ${x}`)
+//     return x + 1
+// }
+
+
+// let y = 5
+// const resolvedY = resolveY().then(val => {
+//     console.log(val)
+// });
+// console.log("Returning value of resolvedY:", resolvedY);
+
 
 
 let A = 3;
@@ -25,15 +32,16 @@ const funcA = async () => {
 }
 
 async function funcB() {
+    await sleep(3000)
     A += 20
     console.log(`set value a in funcB`)
-    await sleep(3000)
+    
 }
 
 async function main() {
-    funcA()
-    funcB()
-    console.logA()
+    await funcA()
+    await funcB()
+    console.log("Value of A:", A)
 }
 
 main()
---
+++ lecture/005-stringLiterals.js
@@ -0,0 +1,6 @@
+console.log("alksdjfladjfk")
+const aVar = 12
+const aString = "this is a string"
+const stringLiteral = `The value of this variable is equal to: ${aVar} this value is oaky`
+console.log("The value of this variable is equal to:", aVar, "this value is oaky")
+console.log(stringLiteral);
